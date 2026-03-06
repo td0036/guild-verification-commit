2d313fd2bb1126d9f2e3bb4bea94fe8fd57eee8f
td0036 <91646762+td0036@users.noreply.github.com>
2026-03-06 23:46:17 +0800

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T15:45:59.615Z
-Commit: Add dark mode support
+Timestamp: 2026-03-06T15:46:16.365Z
+Commit: Resolve null pointer exception in user service
