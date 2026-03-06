98d5874c46e64c9b7649f2e4e502c4ef2a765b3d
td0036 <91646762+td0036@users.noreply.github.com>
2026-03-06 23:46:34 +0800

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-06T15:46:20.520Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-03-06T15:46:32.961Z
+Commit: Fix broken links in documentation
